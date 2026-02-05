85a5e92a3a177545db9b6a704c243185d6c8973f
bryan9514 <btjosueturcios@gmail.com>
2026-02-05 19:48:29 +0100

    ex00 terminado

---
+++ module05/ex00/sources/Bureaucrat.cpp
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 15:18:54 by brturcio          #+#    #+#             */
+/*   Updated: 2026/02/05 19:39:50 by brturcio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+/* =========================== default constructor ========================= */
+Bureaucrat::Bureaucrat(void) :
+	_name("Default"),
+	_grade(150)
+{}
+
+/* =========================== name constructor ============================= */
+Bureaucrat::Bureaucrat(const std::string & name, int grade) :
+	_name(name),
+	_grade(grade)
+{
+	if (_grade < 1)
+		throw (Bureaucrat::GradeTooHighException());
+	if (_grade > 150)
+		throw (Bureaucrat::GradeTooLowException());
+}
+
+/* =========================== copy constructor ============================= */
+Bureaucrat::Bureaucrat(const Bureaucrat & copy)
+{
+	*this = copy;
+}
+
+/* ======================== copy assignment operator ======================== */
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat & other)
+{
+	if (this != &other)
+		_grade = other._grade;
+	return (*this);
+}
+
+/* ================================ destructor ============================== */
+Bureaucrat::~Bureaucrat(void)
+{}
+
+/* ============================ public methods ============================== */
+const std::string	&Bureaucrat::getName(void) const
+{
+	return (_name);
+}
+
+int			Bureaucrat::getGrade(void) const
+{
+	return (_grade);
+}
+
+void	Bureaucrat::increaseGrade(void)
+{
+	if (_grade == 1)
+		throw (Bureaucrat::GradeTooHighException());
+	_grade--;
+}
+
+void	Bureaucrat::decreaseGrade(void)
+{
+	if (_grade == 150)
+		throw (Bureaucrat::GradeTooLowException());
+	_grade++;
+}
+
+const char	*Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high");
+}
+
+const char	*Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low");
+}
+
+std::ostream	&operator<<(std::ostream & str, const Bureaucrat & bureaucrat)
+{
+	str << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade()
+		<< "." << std::endl;
+	return (str);
+}
--- module05/ex00/sources/main.cpp
+++ module05/ex00/sources/main.cpp
@@ -6,13 +6,59 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 12:18:07 by brturcio          #+#    #+#             */
-/*   Updated: 2026/02/05 12:30:40 by brturcio         ###   ########.fr       */
+/*   Updated: 2026/02/05 19:45:23 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <iostream>
+#include "Bureaucrat.hpp"
 
 int	main(void)
 {
-	std::cout << "this is a test\n";
+	std::cout << SUC << "======== Good Bureaucrat  ========\n" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	goodBure("Good", 10);
+
+			std::cout << goodBure << std::endl;
+			goodBure.decreaseGrade();
+			goodBure.decreaseGrade();
+			std::cout << goodBure << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	std::cout << RST;
+	}
+	std::cout << WRN << "\n======== Low Bureaucrat  ========\n" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	lowBure("Low", -100);
+
+			std::cout << "lowBure is: " << lowBure << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	std::cout << RST;
+	}
+	std::cout << ERR << "\n======== High Bureaucrat  ========\n" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	highBure("High", 256);
+
+			std::cout << "highBure is: " << highBure << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	std::cout << RST;
+	}
 	return (0);
 }
