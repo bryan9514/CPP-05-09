f75d5e248f1c187bbc1890a156045ad5d228a32c
Bryan <btjosueturcios@gmail.com>
2026-03-18 10:09:04 +0100

    Miercoles 18-03-26

--- module09/ex00/sources/main.cpp
+++ module09/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 13:28:16 by brturcio          #+#    #+#             */
-/*   Updated: 2026/03/07 13:38:44 by brturcio         ###   ########.fr       */
+/*   Updated: 2026/03/14 16:11:09 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(int ac, char **av)
 {
+	(void)av;
 	if (ac != 2) {
 		std::cout << "Error: could not open file." << std::endl;
 		return (1);
