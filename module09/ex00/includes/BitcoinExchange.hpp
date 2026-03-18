d6a0e2e7f8738a6e7e1c1716eb9bed6e18caa801
bryan9514 <btjosueturcios@gmail.com>
2026-03-18 11:50:07 +0100

    .clang not workig

---
+++ module09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/18 10:23:57 by brturcio          #+#    #+#             */
+/*   Updated: 2026/03/18 11:43:06 by brturcio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <string>
+#include <map>
+
+class BitcoinExchange
+{
+private:
+	std::map<std::string, float>	_data;
+
+public:
+	BitcoinExchange(void);
+	BitcoinExchange(const BitcoinExchange & copy);
+	BitcoinExchange & operator=(const BitcoinExchange & other);
+	~BitcoinExchange(void);
+
+	void	openFile(std::string & name);
+
+};
+
+#endif
