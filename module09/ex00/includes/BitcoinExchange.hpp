12abcc12f5361719607726a0cb12e898da9fa73a
bryan9514 <btjosueturcios@gmail.com>
2026-03-19 12:34:31 +0100

    asd

--- module09/ex00/includes/BitcoinExchange.hpp
+++ module09/ex00/includes/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 10:23:57 by brturcio          #+#    #+#             */
-/*   Updated: 2026/03/18 21:52:10 by brturcio         ###   ########.fr       */
+/*   Updated: 2026/03/19 11:30:04 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,21 @@
 #include <string>
 #include <map>
 
+enum typeError
+{
+	NOT_POSITIVE,
+	NOT_OPEN,
+	BAD_INPUT,
+	TOO_LARGE
+};
+
 class BitcoinExchange
 {
 private:
 	std::map<std::string, float>	_data;
 
 	void	parseLine(std::string & line);
-	void	tokenizer(std::string & line);
+	void	tokenizer(std::string & line, std::string & date, std::string & valor);
 
 public:
 	BitcoinExchange(void);
--- module09/ex00/sources/BitcoinExchange.cpp
+++ module09/ex00/sources/BitcoinExchange.cpp
@@ -6,16 +6,16 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 11:26:26 by brturcio          #+#    #+#             */
-/*   Updated: 2026/03/18 21:52:10 by brturcio         ###   ########.fr       */
+/*   Updated: 2026/03/19 12:34:07 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+#include <cctype>
 #include <cstdio>
 #include <stdexcept>
 #include <fstream>
 #include <iostream>
-#include <vector>
 
 BitcoinExchange::BitcoinExchange(void) :
 	_data()
@@ -44,34 +44,56 @@ BitcoinExchange::~BitcoinExchange(void)
 //
 // }
 
-void	BitcoinExchange::tokenizer(std::string & line)
+void	printfError()
+
+void	BitcoinExchange::tokenizer(std::string & line, std::string & date, std::string & valor)
 {
-	std::vector<std::string>	token;
-	size_t						start = 0;
+	size_t		start = 0;
 
 	for (size_t i = 0; i < line.length(); i++) {
 		if (line[i] == ',') {
 			if (i > start) {
-				token.push_back(line.substr(start, i - start));
+				date = line.substr(start, i - start);
 				start = i + 1;
 			}
 		}
 	}
-	token.push_back(line.substr(start));
-	for (size_t j = 0; j < token.size(); j++) {
-        std::cout << token[j] << std::endl;
+	valor = line.substr(start);
+	for (size_t j = 0; j < 2; j++) {
+        std::cout << date << std::endl;
+        std::cout << valor << std::endl;
 	}
+}
+
+bool	checkDate(std::string)
+{
+	for (int i = 0; date[i] ; i++) {
+		if (!std::isdigit(date[i])) {
+			std::cout << "Error: bad input => 2001-42-42";
+			return (false);
+		}
+	}
+	return (true);
 }
 
 void	BitcoinExchange::loadData(void)
 {
 	std::ifstream	dataFile("data.csv");
+	std::string		date;
+	std::string		valor;
 
 	if (!dataFile)
 		throw std::runtime_error("Error: could not open file.");
 	std::string	line;
 	while (getline(dataFile, line)) {
-		tokenizer(line);
+		tokenizer(line, date, valor);
+		if (!checkDate(date)) {
+			continue ;
+		if (!checkValor(valor)) {
+			std::cout << ""
+		}
+		//checkValor(valor);
+		i++;
 	}
 }
 
